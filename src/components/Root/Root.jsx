ab0a4021571ad6753eda2138d85ee6c140fea54a
abirhossenrafi71 <abirhossenrafi71@gmail.com>
2026-04-16 13:27:52 +0800

    project component setup and header footer design

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,72 @@
+import { Outlet, useNavigation } from "react-router";
+import { ToastContainer } from "react-toastify";
+import Nav from "../NavBar/Nav";
+import Footer from "../Footer/Footer";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import { createContext, useState } from "react";
+
+export const TimelineContext = createContext();
+
+const Root = () => {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
+  const [timelineData, setTimelineData] = useState([]);
+
+  const handleCheckIN = (type, userName) => {
+    const newEntry = {
+      id: Date.now(),
+      type: type,
+      userName: userName,
+      timestamp: new Date().toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
+      date: new Date().toLocaleDateString(),
+    };
+    setTimelineData((prev) => [newEntry, ...prev]);
+  };
+
+  return (
+    <TimelineContext.Provider value={{ timelineData, handleCheckIN }}>
+      <div className="flex flex-col justify-between min-h-screen relative">
+        {isLoading && (
+          <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-white/70 backdrop-blur-[2px]">
+            <span className="loading loading-spinner w-24 text-primary"></span>
+            <h2 className="mt-6 text-2xl z-50 font-bold text-primary animate-pulse">
+              Loading Connections...
+            </h2>
+          </div>
+        )}
+
+        <Nav />
+        <ToastContainer />
+
+        <main className="flex-1 container mx-auto px-6 py-10">
+          
+          {isLoading ? (
+            <div className="w-full">
+              <Skeleton height={40} width="30%" className="mb-6" />
+              <Skeleton height={250} borderRadius={16} className="mb-10" />
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                {[1, 2, 3].map((n) => (
+                  <div key={n} className="p-5 rounded-xl">
+                    <Skeleton height={150} className="mb-4" />
+                    <Skeleton count={2} />
+                  </div>
+                ))}
+              </div>
+            </div>
+          ) : (
+            <Outlet />
+          )}
+        </main>
+
+        <Footer />
+      </div>
+    </TimelineContext.Provider>
+  );
+};
+
+export default Root;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,58 @@
-import { StrictMode } from 'react'
+import { Children, Component, StrictMode } from 'react'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"; 
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Root from './components/Root/Root'
+import Home from './components/Home/Home'
+import Timeline from './components/Timeline/Timeline'
+import Status from './components/Status/Status'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: Root,
+
+
+    children: [
+      {
+        index: true,
+        loader: () => fetch("/friends.json"),
+        Component: Home
+      },
+      {
+        path:"home",
+        loader: () => fetch("/friends.json"),
+        Component: Home
+      },
+      {
+        path: "timeline",
+        loader: () => fetch("/friends.json"),
+        Component: Timeline
+      },
+      {
+        path: "status",
+        loader: () => fetch("/friends.json"),
+        Component: Status
+      },
+      // {
+      //   path: "userDetails/:userID",
+      //   loader: async ({ params }) => {
+      //     const response = await fetch("/friends.json");
+      //     const data = await response.json();
+      //     const singleUser = data.find((user) => user.id == params.userID);
+      //     if (!singleUser) {
+      //       throw new Response("User Not Found", { status: 404 });
+      //     }
+      //     return singleUser;
+      //   },
+      //   Component: UserDetails,
+      // },
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
